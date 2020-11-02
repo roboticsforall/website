d9a369d31d8dcacbb851eb9c6bffe76179ec5fda
Arvind G <60164133+DevSandBoxx@users.noreply.github.com>
2020-11-02 11:31:25 -0600

    Testing Commits

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,7 +10,7 @@ export default class App extends React.Component{
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and bing bing bong bong.
           </p>
           <a
             className="App-link"
