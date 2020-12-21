f4c3aef572eb4487c6df7a40ae42d947d6280ba0
Arvind G <60164133+DevSandBoxx@users.noreply.github.com>
2020-12-21 15:28:46 -0600

    adding cards to afforgs

--- src/components/About/RFAAffOrgs.js
+++ src/components/About/RFAAffOrgs.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Container, Row, Image, Col, Card, Button} from "react-bootstrap";
+import {Container, Row, Image, Col, Card, CardDeck} from "react-bootstrap";
 import RFAAffOrgsCard from "../RFAAffOrgsCard";
 
 export default function RFAAffOrgs(props){
@@ -8,15 +8,15 @@ export default function RFAAffOrgs(props){
           <Row className = "header-blobs">
             <Container className = "d-flex align-self-end">
               <Row className = "m-5 w-100 justify-content-center">
-                <h1 className = "page-header">Check out our Affiliate Organizations!</h1>
-                <p className = "page-header-description">Learn more about our affiliate organizations started from members of Robotics for All!</p>
+                <h1 className = "page-header header-size">Check out our Affiliate Organizations!</h1>
+                <p className = "page-header-description header-description-size">Learn more about our affiliate organizations started from members of Robotics for All!</p>
               </Row>
             </Container>
           </Row>
           <Container>
-            <RFAAffOrgsCard />
-            <RFAAffOrgsCard />
-            <RFAAffOrgsCard />
+            <RFAAffOrgsCard affOrgName = {"Tutoring For All"}/>
+            <RFAAffOrgsCard affOrgName = {"Mentoring For All"}/>
+            <RFAAffOrgsCard affOrgName = {"Crafts For Charity"}/>
           </Container>
           </Row>
     );
--- src/components/RFAAffOrgsCard.js
+++ src/components/RFAAffOrgsCard.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import {Container, Row, Image, Col, Card, Button} from "react-bootstrap";
+import {Container, Row, Image, Col, Card, CardDeck} from "react-bootstrap";
 import MFALogo from "../media/AffiliatedOrgs/MFALogo.png";
 
 export default function RFAAffOrgsCard(props){
     return (
-        <Row className = "mt-5 border">
-            <Container>
-            <Row><h1>Tutoring For All</h1></Row>
-            <Row className = "d-flex justify-content-center align-items-center border">
-                <Col>
-                <Image src = {MFALogo} fluid/>
+        <Row>
+            <h1 className = "header-size">{props.affOrgName}</h1>
+            <Row className = "justify-content-center">
+                <Col md = {3} className = "border">
+                    <Container>
+                        <Image src = {MFALogo} fluid/>
+                    </Container>
                 </Col>
-                {/* <Image src = {MFALogo} fluid/> */}
-                <Col>
+                <Col md = {4} className = "border">
+                    <Container>
+                        <Image src = {MFALogo} fluid/>
+                    </Container>
                 </Col>
-                <Col>
-                <Row>
-                    <p>Tutoring for All is a branch of the non-profit organization Robotics for All. Our goal is to provide free, quality tutoring services to primarily low income and disadvantaged students.</p>
-                </Row>
-                <Row><a href = "">Visit MFA</a></Row>
+                <Col md = {5} className = "d-flex align-items-center border">
+                    <Container>
+                        <Row>Tutoring for All is a branch of the non-profit organization Robotics for All. Our goal is to provide free, quality tutoring services to primarily low income and disadvantaged students.</Row>
+                        <Row className = "justify-content-center"><a className = "text-center" href = "#">Vist TFA</a></Row>
+                    </Container>
                 </Col>
             </Row>
-            </Container>
+            <br/><br/>
         </Row>
     );
 }
