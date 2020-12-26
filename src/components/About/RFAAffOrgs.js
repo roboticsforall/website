e04c14b7d5d95a6dcc9c3fd49716655007c4664e
Arvind G <60164133+DevSandBoxx@users.noreply.github.com>
2020-12-25 22:39:44 -0600

    worked on cards

--- src/components/About/RFAAffOrgs.js
+++ src/components/About/RFAAffOrgs.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container, Row, Image, Col, Card, CardDeck} from "react-bootstrap";
 import RFAAffOrgsCard from "../RFAAffOrgsCard";
 
@@ -8,16 +9,16 @@ export default function RFAAffOrgs(props){
           <Row className = "header-blobs">
             <Container className = "d-flex align-self-end">
               <Row className = "m-5 w-100 justify-content-center">
-                <h1 className = "page-header header-size">Check out our Affiliate Organizations!</h1>
-                <p className = "page-header-description header-description-size">Learn more about our affiliate organizations started from members of Robotics for All!</p>
+                <h1 style = {{color: "#FFCC00"}} className = "page-header header-size text-center">Check out our Affiliate Organizations!</h1>
+                <p className = "page-header-description header-description-size text-center">Learn more about our affiliate organizations started from members of Robotics for All!</p>
               </Row>
             </Container>
           </Row>
           <Container>
-            <RFAAffOrgsCard affOrgName = {"Tutoring For All"}/>
-            <RFAAffOrgsCard affOrgName = {"Mentoring For All"}/>
-            <RFAAffOrgsCard affOrgName = {"Crafts For Charity"}/>
+            <RFAAffOrgsCard bgColor = {""} color = {"#3399ff"} affOrgName = {"Tutoring For All"} affOrgsDescription = {"Tutoring for All is a branch of the non-profit organization Robotics for All. Our goal is to provide free, quality tutoring services to primarily low income and disadvantaged students."}/>
+            <RFAAffOrgsCard color = {"#33cc66"} affOrgName = {"Mentoring For All"} affOrgsDescription = {"Mentoring for All is a branch of the non-profit organization Robotics for All with the goal of providing guidance on the path towards future success for underserved high school students."}/>
+            <RFAAffOrgsCard color = {"#fa8d1f"} affOrgName = {"Crafts For Charity"} affOrgsDescription = {"Crafts for Charity is a student-led nonprofit providing classes and opportunities for everyone to give back to their communities through crafting! We help donate handmade goods to charities for causes like homelessness."}/>
           </Container>
-          </Row>
+        </Row>
     );
 }
--- src/components/RFAAffOrgsCard.js
+++ src/components/RFAAffOrgsCard.js
@@ -5,21 +5,17 @@ import MFALogo from "../media/AffiliatedOrgs/MFALogo.png";
 export default function RFAAffOrgsCard(props){
     return (
         <Row>
-            <h1 className = "header-size">{props.affOrgName}</h1>
-            <Row className = "justify-content-center">
-                <Col md = {3} className = "border">
-                    <Container>
-                        <Image src = {MFALogo} fluid/>
-                    </Container>
+            <h1 style = {{color: props.color}} className = "header-size afforgs-header">{props.affOrgName}</h1>
+            <Row style = {{backgroundColor: (props.color + "80"), borderRadius: 15}} className = "justify-content-center">
+                <Col md = {3} className = "p-3 border d-flex align-items-center justify-content-center ">
+                    <Image src = {MFALogo} fluid/>
                 </Col>
-                <Col md = {4} className = "border">
-                    <Container>
-                        <Image src = {MFALogo} fluid/>
-                    </Container>
+                <Col md = {4} className = "p-3 border d-flex align-items-center justify-content-center">
+                    <Image src = {MFALogo} fluid/>
                 </Col>
-                <Col md = {5} className = "d-flex align-items-center border">
+                <Col md = {5} className = "p-3 border d-flex align-items-center">
                     <Container>
-                        <Row>Tutoring for All is a branch of the non-profit organization Robotics for All. Our goal is to provide free, quality tutoring services to primarily low income and disadvantaged students.</Row>
+                        <Row><p className = "afforgs-card-text text-center">{props.affOrgsDescription}</p></Row>
                         <Row className = "justify-content-center"><a className = "text-center" href = "#">Vist TFA</a></Row>
                     </Container>
                 </Col>
