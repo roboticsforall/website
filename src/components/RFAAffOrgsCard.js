37acc9790f6e2ea7589f38dc9387ebbc41edef8b
Arvind G <60164133+DevSandBoxx@users.noreply.github.com>
2021-01-02 16:18:07 -0600

    fsdf

--- src/components/RFAAffOrgsCard.js
+++ src/components/RFAAffOrgsCard.js
@@ -43,11 +43,11 @@ export default class RFAAffOrgsCard extends React.Component{
                         </Col>
                     ) : (
                         <Col>
-                            <Row className = "p-3">
-                                <Col sm = {5} md = {5} lg = {5} xl = {5}className = "d-flex align-items-center justify-content-center ">
+                            <Row className = "p-3 justify-content-around ">
+                                <Col sm = {5} md = {5} lg = {5} xl = {5} className = "d-flex align-items-center">
                                     <Image className = "aff-orgs-logo-size" src = {this.props.logo}/>
                                 </Col>
-                                <Col sm = {7} md = {7} lg = {7} xl = {7} className = "d-flex align-items-center justify-content-center ">
+                                <Col sm = {7} md = {7} lg = {7} xl = {7} className = "d-flex align-items-center">
                                     <h1 style = {{color: this.props.mainColor}} className = "text-center header-size afforgs-header">{this.props.affOrgName}</h1>
                                 </Col>
                             </Row>
