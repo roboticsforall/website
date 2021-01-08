9d59c125950dc70bc59a6b61ec30e0fc5bc5fc7d
sanjayr47 <66031667+sanjayr47@users.noreply.github.com>
2021-01-08 07:37:19 -0800

    Finished the rest of homepage

--- src/App.js
+++ src/App.js
@@ -1,47 +1,164 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Container, Row} from 'react-bootstrap';
-import RFANavbar from "./components/RFANavbar";
-// import RFACard from "./components/RFACard";
-import RFAAffOrgs from "./components/About/RFAAffOrgs";
-export default class App extends React.Component{
+//import {Container, Row} from 'react-bootstrap';
+export default class App extends React.Component {
 
   render() {
     return (
-      <Container className = "border" fluid>
-        <Row> {/* Structure of webpage will follow grid layout => row, column, container */}
-          <Container>
-              <header>
-                  <RFANavbar/>
-              </header>
-          </Container>
-        </Row>
-        <RFAAffOrgs/>
 
-        {/* <Container>
-          <Row>
-            {
-              ["one", "two", "three"].map(element => (
-                <Col>
-                  Image
-                  Text
-                </Col>
-              ))
-            }
-          </Row>
-        </Container>
-        <Container>
-          <Row>
-            <CardDeck>
-              <RFACard />
-              <RFACard />
-              <RFACard />
-              <RFACard />
-            </CardDeck>
-          </Row>
-        </Container> */}
-      </Container>
+
+      <center>
+        <div id="container">
+          <img src="https://www.roboticsforall.net/uploads/1/1/5/4/115434321/img-9211_orig.jpg" />
+          <div id="backT">
+            <h1>At Robotics For All, we believe everyone deserves access to quality STEM enrichment programs. </h1>
+          </div>
+        </div>
+
+        <a href="#">Click here to view updates from Robotics for All on the COVID-19 outbreak.</a>
+
+        <p>All classes right now are online due to the COVID-19 pandemic.</p>
+
+        <hr></hr>
+        <h2><b>What is Robotics For All?</b></h2>
+
+        <p>Robotics For All provides free STEM education to students of all backgrounds across the nation, particularly students from underserved communities. With fun and intuitive curricula, we aim to expose children to the field of STEM and robotics, and inspire them to pursue STEM later in their lives. We provide curriculum and support high school and college volunteers to teach our classes and engage with their local communities.</p>
+        <hr></hr>
+        <div id="stats">
+          <div class="num">
+            <h1><b>1085</b></h1>
+            <p>K-8th Graders Taught Over Online (557) and In-Person Classes (528)</p>
+          </div>
+          <div class="num">
+            <h1><b>202</b></h1>
+            <p>Active Volunteers
+(Teachers, Developers, and More)</p>
+          </div>
+          <div class="num">
+            <h1><b>12</b></h1>
+                Levels of Curriculum
+​(8 Online, 4 In-Person)
+              </div>
+
+        </div>
+
+        <br></br>
+
+        <p>---------------------------
+
+</p>
+        <hr></hr>
+        <p><i>"The class was so much fun. I loved it"</i></p>
+        <p><b>STUDENT</b>, Fifth Grade</p>
+
+        <br></br>
+
+        <p><i>“ The joy on Max's face translated to the students' faces so easily. His excitement became their excitement ”
+            </i></p>
+        <p><b>KATHY SHIONIS</b>, School Administrator
+</p>
+
+        <br></br>
+
+        <p><i>“ The teachers are knowledgable and enthusiastic... We are truly grateful for this program and will definitely recommend it to others with interest in coding. Thank you!" ”
+            </i></p>
+        <p><b>PARENT</b></p>
+        <hr></hr>
+        <div>
+          <div class="news">
+            <h3>News</h3>
+            <p><a href="#">Read more about about RFA!</a></p>
+          </div>
+          <div class="news">
+            <button>LEARN MORE</button>
+          </div>
+
+        </div>
+        <div>
+          <h2>---</h2>
+          <img src="https://www.roboticsforall.net/uploads/1/1/5/4/115434321/pvsa-gold_orig.png" />
+          <p><a href="#">Would you like a President's Volunteer Service Award? Click here for more info
+</a></p>
+        </div>
+        <hr></hr>
+        <h2>Invited Talks and Fundraising</h2>
+        <hr></hr>
+        <div>
+
+          <div class="talks">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/aYSU-1hKHiY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <h4>BEAM Teen Entrepreneurship Conference</h4>
+            <p>Founder and CEO of Robotics for All, Maximilian Goetz, was honored to be the guest speaker at the 2019 BEAM Teen Entrepreneurship Conference on March 14th, 2019.</p>
+
+          </div>
+
+          <div class="talks">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/jHhpK_vlJcQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <h4>Next Generation Nations Global Challenges Competition
+</h4>
+            <p>Maximilian Goetz and Garrett Tieng pitched at the 2019 Next Generation Nations Global Challenges Competition, winning second place with a cash prize of $1,500.</p>
+
+          </div>
+
+          <div class="talks">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/IqgSK39czO0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <h2>Jugend Hackt Hackathon </h2>
+            <p>Maximilian Goetz was invited on an expenses paid trip to speak at the 2019 Judgend Hackt Hackathon in Heidelberg, Germany, on October 19th, 2019.
+</p>
+
+          </div>
+
+          <div class="talks">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/xK1wg0mwJP8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <h2>TEDx Gunn High School</h2>
+            <p>Maximilian Goetz was one of eight speakers at TEDxGunnHighSchool on January 17th, 2020.
+</p>
+
+          </div>
+
+        </div>
+
+
+       <div>
+
+       <p>--------------------------------------------------------------</p>
+       <br>
+       </br>
+       <h3>Supported by the following organizations and over 50 Independent donors:</h3>
+       <div>
+        <div class="donor"><img class="donimg" src="https://www.roboticsforall.net/uploads/1/1/5/4/115434321/lockheed-martin-logo_orig.png"></img></div>
+        <div class="donor"><img class="donimg" src="https://www.roboticsforall.net/uploads/1/1/5/4/115434321/download-1_orig.jpeg"></img></div>
+        <div class="donor"><img class="donimg" src="https://www.roboticsforall.net/uploads/1/1/5/4/115434321/published/product-image.png?1577160522"></img></div>
+
+        <div class="donor"><img class="donimg" src="https://www.roboticsforall.net/uploads/1/1/5/4/115434321/published/think-fund-1-rgb-logo_1.png?1577160529"></img></div>
+        <div class="donor"><img class="donimg" src="https://www.roboticsforall.net/uploads/1/1/5/4/115434321/weekend-max-mara-logo-white_1_orig.jpg"></img></div>
+        <div class="donor"><img class="donimg" src="https://www.roboticsforall.net/uploads/1/1/5/4/115434321/published/google2-0-0.jpg?1607650053"></img></div>
+       </div>
+       </div>
+      
+       <div>
+
+
+
+<p>---------------------------</p>
+<h3>​Proud partners of the following organizations:
+</h3>
+<div>
+ <div class="donor2"><img class="donimg" src="https://www.roboticsforall.net/uploads/1/1/5/4/115434321/editor/new-ngn-logo-2019-grey-ring-white-background-png.png?1577160514"></img></div>
+ <div class="donor2"><img class="donimg" src="https://www.roboticsforall.net/uploads/1/1/5/4/115434321/screen-shot-2020-06-11-at-6-40-44-pm_orig.png"></img></div>
+ <div class="donor2"><img class="donimg" src="https://www.roboticsforall.net/uploads/1/1/5/4/115434321/published/ymca-logo-logotype.png?1607883585"></img></div>
+
+ <div class="donor2"><img class="donimg" src="https://www.roboticsforall.net/uploads/1/1/5/4/115434321/published/wilmer-hale-logo.jpg?1607650422"></img></div>
+ </div>
+</div>
+
+
+      </center>
+
+
+
+
     );
   }
 }
