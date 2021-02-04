54a5da9dca7a175199568b3f9695480989f4990f
Arvind G <60164133+DevSandBoxx@users.noreply.github.com>
2021-02-04 08:59:56 -0600

    Update RFAHeader.js

--- src/components/RFAHeader.js
+++ src/components/RFAHeader.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import {Container, Row, Col} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function RFAHeader(props){
   
     return (
         <Row style = {{backgroundImage: `url(${props.image})`}} className = "header-blobs">
-          <Container className = "d-flex align-self-end">
+          <Container className = "align-self-end">
             <Col className = "border mb-6 w-100">
-              <Row className = "text-center">
-                <h1 style = {{color: props.headerTextColor}} className = "header-size page-header">{props.title}</h1>
+              <Row className = "justify-content-center">
+                <h1 style = {{color: props.headerTextColor}} className = "text-center header-size page-header">{props.title}</h1>
               </Row>
               <Row className = "justify-content-center">
-                <p className = "header-description-size page-header-description">{props.description}</p>
+                <p className = "text-center header-description-size page-header-description">{props.description}</p>
               </Row>
             </Col>
           </Container>
