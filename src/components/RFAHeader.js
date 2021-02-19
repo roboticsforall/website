f491e2620d6a989544090752a743cb43fba1b11e
Arvind G <60164133+DevSandBoxx@users.noreply.github.com>
2021-02-19 08:22:07 -0600

    moved css page classes to in component

--- src/components/RFAHeader.js
+++ src/components/RFAHeader.js
@@ -4,15 +4,32 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function RFAHeader(props){
   
+    const pageHeaderDescription = {
+      fontFamily: 'BeVietnam-Medium',
+      fontWeight: 600,
+      color: "black",
+    }
+    const pageHeader = {
+      fontFamily: "Mazzard-H-Extrabold",
+      transform: 'rotate(358deg)',
+      color: props.headerTextColor,
+    }
+    const headerBlob = {
+      backgroundSize: "100% 100%",
+      height: "45vw",
+      width: "100vw",
+      backgroundImage: `url(${props.image})`,
+    }
+
     return (
-        <Row style = {{backgroundImage: `url(${props.image})`}} className = "header-blobs">
+        <Row style = {headerBlob}>
           <Container className = "align-self-end">
             <Col className = "border mb-6 w-100">
               <Row className = "justify-content-center">
-                <h1 style = {{color: props.headerTextColor}} className = "text-center header-size page-header">{props.title}</h1>
+                <h1 style = {pageHeader} className = "text-center header-size">{props.title}</h1>
               </Row>
               <Row className = "justify-content-center">
-                <p className = "text-center header-description-size page-header-description">{props.description}</p>
+                <p style = {pageHeaderDescription} className = "text-center header-description-size">{props.description}</p>
               </Row>
             </Col>
           </Container>
