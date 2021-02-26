1b17f84ef01c2a23c6c44676c5f56eac4098355c
phoebechen2005 <74422531+phoebechen2005@users.noreply.github.com>
2021-02-25 19:57:52 -0500

    Update Components

--- src/components/RFAPositionCard.js
+++ src/components/RFAPositionCard.js
@@ -1,1 +1,23 @@
+import React from 'react';
+import {Container, Row, Col} from "react-bootstrap";
 
+export default class RFAPositionCard extends React.Component{
+
+    render () {
+
+    return (
+            <Container className = "pos-container">
+                <h1 className = "pos-header">{this.props.posHeader}</h1>
+                <p className = "pos-description">
+                    <div>
+                        <Row>
+                            <Col>
+                                {this.props.posDescription}
+                            </Col>
+                        </Row>
+                    </div>
+                </p>
+            </Container>
+    );
+    }
+}
--- src/components/RFAPositionCard2.js
+++ src/components/RFAPositionCard2.js
@@ -1,1 +1,26 @@
+import React from 'react';
+import {Container, Row, Col, Image} from "react-bootstrap";
 
+export default class RFAPositionCard2 extends React.Component{
+
+    render () {
+
+    return (
+            <Container className = "pos-container">
+                <h1 className = "pos-header">{this.props.posHeader}</h1>
+                <p className = "pos-description">
+                    <div>
+                        <Row>
+                            <Col>
+                                {this.props.posDescription}
+                            </Col>
+                            <Col  xs = {3}>
+                                <Image className = "justify-content-center" src = {this.props.posImage} fluid />
+                            </Col>
+                        </Row>
+                    </div>
+                </p>
+            </Container>
+    );
+    }
+}
