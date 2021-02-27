743bf9d6ed2307fc4a273654852757c012af8aac
Arvind G <60164133+DevSandBoxx@users.noreply.github.com>
2021-02-26 22:21:00 -0600

    Update RFATemplateWebpage.js

--- src/components/RFATemplateWebpage.js
+++ src/components/RFATemplateWebpage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Container, Row} from "react-bootstrap";
 import RFAHeader from './RFAHeader';
-import headerBlobYellow from '../media/HeaderBlobs/HBLightYellow.png'; // add correct image and filepath here
+import headerBlobYellow from '../media/HeaderBlobs/Blob-RFA-4.png'; // add correct image and filepath here
 
 export default function RFAAddWebpage(props){
     return (
