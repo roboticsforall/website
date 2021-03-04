0b175dd962293adb58e594b34bee29b6e263fa23
k4tie113 <katieli167@gmail.com>
2021-03-03 17:04:39 -0800

    standardized files

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './RFAVolOverview.js';
+import App from './App.js';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
