ff99bb123d366465ade415ef1aa39ad23120b317
Gina Park <gina00@live.unc.edu>
2021-03-06 15:41:51 -0500

    adding google form

--- src/components/RFAPositionCard.js
+++ src/components/RFAPositionCard.js
@@ -5,18 +5,30 @@ export default class RFAPositionCard extends React.Component{
     
     render () {
 
+        const posHeader = {
+            fontFamily: "Mazzard-H-Extrabold",
+            letterSpacing: "0.25em",
+            textAlign: "center",
+        };
+
+        const posDescription = {
+            color: "#000000",
+        };
+
+        const posContainer = {
+            backgroundColor: "#ffeb99"
+        };
+
     return (
-            <Container className = "pos-container">
-                <h1 className = "pos-header">{this.props.posHeader}</h1>                    
-                <p className = "pos-description">
-                    <div>
-                        <Row>
-                            <Col>
-                                {this.props.posDescription}
-                            </Col>
-                        </Row>
-                    </div>
-                </p>   
+            <Container className = "pos-container" style = {posContainer}>
+                <div>                
+                    <Row>
+                        <Col>
+                            <h1 className = "header-size" style = {posHeader}>{this.props.posHeader}</h1>    
+                            <p className = "header-description-size" style = {posDescription}>{this.props.posDescription}</p>
+                        </Col>
+                    </Row>
+                </div>
             </Container>
     );
     }
