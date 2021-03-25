160b3a173bc7b225cc4c6699abadf4c1c478b250
k4tie113 <katieli167@gmail.com>
2021-03-24 20:17:22 -0700

    added header blob and standardized folders

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './overview.js';
+import App from "./components/About/RFAAbtOverview";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
