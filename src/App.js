cc841f8a1bca86e9dd7bda8e9e042a76ca641466
Arvind G <60164133+DevSandBoxx@users.noreply.github.com>
2021-03-31 10:13:05 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container, Row} from 'react-bootstrap';
 import RFANavbar from "./components/RFANavbar";
+<<<<<<< Updated upstream
 import RFAHomePage from './components/RFAHomePage';
 import RFAFooter from "./components/RFAFooter";
+=======
+// import RFACard from "./components/RFACard";
+import RFAAddWebpage from './components/RFAAddWebpage';
+>>>>>>> Stashed changes
 
 export default class App extends React.Component{
 
