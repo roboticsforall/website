a7c6121d2daf9393a27ea84e653b5d7cadbf4342
Gina Park <gina00@live.unc.edu>
2021-04-13 14:38:11 -0400

    removed bylaw entries

--- src/components/RFACorpBylaws.js
+++ src/components/RFACorpBylaws.js
@@ -35,30 +35,6 @@ return (
         </Container>
         <Container style ={background}>
             <RFABylawEntry date = {"December 1st, 2018"} bylaw = {"Initial Incorporation Bylaws"} download = {"#"}/>
-            <RFABylawEntry date = {"December 8th, 2018"} bylaw = {"Amendment 1: Chairperson"} download = {"#"}/>
-            <RFABylawEntry date = {"December 8th, 2018"} bylaw = {"Amendment 2: Voting Online"} download = {"#"}/>
-            <RFABylawEntry date = {"January 20th, 2019"} bylaw = {"Amendment 3: Remove Membership Clause"} download = {"#"}/>
-            <RFABylawEntry date = {"February 17th, 2019"} bylaw = {"Amendment 4: Graphic Designer"} download = {"#"}/>
-            <RFABylawEntry date = {"April 21st, 2019"} bylaw = {"Amendment 5: Executive Positions"} download = {"#"}/>
-            <RFABylawEntry date = {"April 21st, 2019"} bylaw = {"Amendment 6: Advisory Committee"} download = {"#"}/>
-            <RFABylawEntry date = {"April 21st, 2019"} bylaw = {"Amendment 7: Local Director Committee"} download = {"#"}/>
-            <RFABylawEntry date = {"May 3rd, 2019"} bylaw = {"Amendment 8: Section 5.2 - Number, Tenure, and Qualifications"} download = {"#"}/>
-            <RFABylawEntry date = {"May 3rd, 2019"} bylaw = {"Amendment 9: Section 5.2 - Number, Tenure, and Qualifications"} download = {"#"}/>
-            <RFABylawEntry date = {"June 2nd, 2019"} bylaw = {"Amendment 10: Removal of Board Advisor Position"} download = {"#"}/>
-            <RFABylawEntry date = {"June 2nd, 2019"} bylaw = {"Amendment 11: Removal of Publicity Committee"} download = {"#"}/>
-            <RFABylawEntry date = {"June 2nd, 2019"} bylaw = {"Amendment 12: Addition of CFO"} download = {"#"}/>
-            <RFABylawEntry date = {"July 28th, 2019"} bylaw = {"Amendment 13: Online Voting"} download = {"#"}/>
-            <RFABylawEntry date = {"October 4th, 2020"} bylaw = {"Amendment 14: Number of Board Members"} download = {"#"}/>
-            <RFABylawEntry date = {"October 4th, 2020"} bylaw = {"Amendment 15: Number of Board Members and New Positions"} download = {"#"}/>
-            <RFABylawEntry date = {"October 19th, 2020"} bylaw = {"Amendment 16: Director of Recruitment"} download = {"#"}/>
-            <RFABylawEntry date = {"October 19th, 2020"} bylaw = {"Amendment 17: Finance Committee"} download = {"#"}/>
-            <RFABylawEntry date = {"October 19th, 2020"} bylaw = {"Amendment 18: Publicity Committee"} download = {"#"}/>
-            <RFABylawEntry date = {"October 19th, 2020"} bylaw = {"Amendment 19: Curriculum Committee"} download = {"#"}/>
-            <RFABylawEntry date = {"November 15th, 2020"} bylaw = {"Amendment 20: Fiscal Year Change"} download = {"#"}/>
-            <RFABylawEntry date = {"November 15th, 2020"} bylaw = {"Amendment 21: Addition of Legal Affairs Director"} download = {"#"}/>
-            <RFABylawEntry date = {"November 15th, 2020"} bylaw = {"Amendment 22: Board Member Reshuffling and Compensation"} download = {"#"}/>
-            <RFABylawEntry date = {"November 15th, 2020"} bylaw = {"Amendment 23: Incumbent Board Member Reapplication Process"} download = {"#"}/>
-            <RFABylawEntry date = {"November 15th, 2020"} bylaw = {"Amendment 24: Executive Officer Compensation"} download = {"#"}/>
         </Container>
     </div>
 );
