eed6d4b333e62622652bd699fc6f39d96be19f72
Gina Park <gina00@live.unc.edu>
2021-04-13 14:40:47 -0400

    removed meeting entries

--- src/components/RFAMeetingRecords.js
+++ src/components/RFAMeetingRecords.js
@@ -35,40 +35,6 @@ return (
         </Container>
         <Container style ={background}>
             <RFAMeetingEntry date = {"December 1st, 2018"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"December 8th, 2018 (Special Meeting)"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"January 20th, 2019"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"February 17th, 2019"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"March 10th, 2019"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"April 21st, 2019"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"May 2019"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"June 2nd, 2019"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"July 22nd, 2019"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"August 31st, 2019"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"October 12th, 2019"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"November 10th, 2019"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"December 8th, 2019"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"January 24th, 2020"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"Feburary 13th, 2020"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"Feburary 29th, 2020 (Special Meeting)"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"March 15th, 2020"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"April 5th, 2020"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"May 3rd, 2020"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"June 7th, 2020"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"July 28th, 2020 (Special Board Meeting)"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"August 2nd, 2020"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"August 11th, 2020 (Special Board Meeting)"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"September 16th, 2020"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"October 4th, 2020"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"October 18th, 2020"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"October 29th, 2020 (Special Board Meeting)"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"November 5th, 2020 (Special Board Meeting)"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"November 15th, 2020"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"December 4th, 2020 (Special Board Meeting)"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"December 6th, 2020"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"December 19th, 2020"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"January 14th, 2021"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"January 24th, 2021"} agenda = {"#"} minutes = {"#"}/>
-            <RFAMeetingEntry date = {"February 9th, 2021"} agenda = {"#"} minutes = {"#"}/>
         </Container>
     </div>
 );
