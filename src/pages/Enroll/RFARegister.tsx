5e969212260df9c84f3dcff8f0fd18a2eaaec6c8
Arvind G <60164133+DevSandBoxx@users.noreply.github.com>
2021-06-29 14:10:31 -0500

    removing question

--- src/pages/Enroll/RFARegister.tsx
+++ src/pages/Enroll/RFARegister.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { RFAHeader } from "@/components/RFAHeader";
 import headerBlobBlue from "@/media/HeaderBlobs/blue.png"; // add correct image and filepath here
-import registerFormJSON from "@/posts/register_form.json";
+import googleForms from "@/posts/google_form.json";
 import Markdown from "markdown-to-jsx";
 
 export const RFARegister: React.FC = () => {
@@ -23,17 +23,7 @@ export const RFARegister: React.FC = () => {
       <Container>
         <Row>
           <Col className="d-flex justify-content-center">
-            {/* <iframe
-              src="https://docs.google.com/forms/d/e/1FAIpQLScZ-YvhK6guV9n7M8wn31TYpGBtSC9QpyOSzjQPoM_Db683NA/viewform?embedded=true"
-              width="640"
-              height="975"
-              frameBorder="0"
-              marginHeight={0}
-              marginWidth={0}
-            >
-              Loading…
-            </iframe> */}
-            <Markdown>{registerFormJSON.register_form_code.code}</Markdown>
+            <Markdown>{googleForms.register_form_code.code}</Markdown>
           </Col>
         </Row>
         <br></br>
