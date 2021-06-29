447246e509562baa7538b20daf6d1bd122124487
Arvind G <60164133+DevSandBoxx@users.noreply.github.com>
2021-06-29 13:58:43 -0500

    removing question

--- src/pages/Volunteer/RFAPublicityInternPos.tsx
+++ src/pages/Volunteer/RFAPublicityInternPos.tsx
@@ -14,6 +14,9 @@ import TasksYellow from "@/media/Graphics/tasksYellow.png";
 import ThumbsUp from "@/media/Graphics/thumbsUp.png";
 import QuestionMark from "@/media/Graphics/questionMark.png";
 
+import pubInternOverviewJSON from "@/posts/pub_intern_overview.json";
+import Markdown from "markdown-to-jsx";
+
 export const RFAPublicityInternPos: React.FC = () => {
   const [key, setKey] = useState("one");
   const ActiveStyle = {
@@ -73,16 +76,7 @@ export const RFAPublicityInternPos: React.FC = () => {
                 posDescription={
                   <div>
                     <p>
-                      Publicity Interns are specialized Business Development
-                      Interns, who are specifically dedicated to helping the
-                      publicity team. These volunteers are super important to
-                      our organization, as they increase the professionalism and
-                      personality of our whole organization with their talents.
-                      You may apply in one of two categories: Graphic Design
-                      (Puts graphics together into finished products like
-                      Instagram posts, flyers, and much more), and Illustrators
-                      (Illustrates new graphics). Please only apply if you are
-                      skilled in one of the two above categories.
+                      <Markdown>{pubInternOverviewJSON.overview}</Markdown>
                     </p>
                   </div>
                 }
@@ -92,31 +86,7 @@ export const RFAPublicityInternPos: React.FC = () => {
                 posImage={TasksYellow}
                 posDescription={
                   <div>
-                    <p>
-                      You are not required to know how to do all of these tasks,
-                      but you must be able to do at least one specific task.
-                    </p>
-                    <ul>
-                      <li>
-                        Illustrate custom graphics for Robotics for All’s
-                        website and graphic design team
-                      </li>
-                      <li>
-                        Proficient in graphic design. Create various assemblies
-                        of graphics including things like flyers, social media
-                        posts, and more
-                      </li>
-                      <li>
-                        Crafting short stories and videos for Robotics for All
-                      </li>
-                      <li>
-                        Build connections with corporate companies and
-                        organizations to form partnerships, host events and more
-                      </li>
-                      <li>
-                        Other various Business Development Internship tasks
-                      </li>
-                    </ul>
+                      <Markdown>{pubInternOverviewJSON.tasks}</Markdown>
                   </div>
                 }
               />
@@ -124,75 +94,28 @@ export const RFAPublicityInternPos: React.FC = () => {
                 posHeader={"TIME COMMITMENT"}
                 posImage={Clock}
                 posDescription={
-                  <ul>
-                    <li>
-                      The minimum time commitment for this position is 3 hours
-                      per week, for one month
-                    </li>
-                    <li>
-                      The total time commitment varies depending on which tasks
-                      you take and time of year
-                    </li>
-                    <li>Interns are expected to attend bimonthly meetings</li>
-                  </ul>
+                  <Markdown>{pubInternOverviewJSON.time_commitment}</Markdown>
                 }
               />
               <RFAPositionCard2
                 posHeader={"BENEFITS"}
                 posImage={ThumbsUp}
                 posDescription={
-                  <ul>
-                    <li>
-                      This is an unpaid position. Due to that, we can offer
-                      community service hours
-                    </li>
-                    <li>
-                      Interning with Robotics for All looks great on any resume,
-                      whether applying for a future internship, job, or college
-                    </li>
-                    <li>
-                      PotPotential future leadership opportunities (Robotics for
-                      All board, etc.) may be offered.
-                    </li>
-                    <li>
-                      Experience the satisfaction of improving your local
-                      community.
-                    </li>
-                    <li>
-                      Earn a recommendation letter from one of Robotics for
-                      All's leadership members.
-                    </li>
-                  </ul>
+                  <Markdown>{pubInternOverviewJSON.benefits}</Markdown>
                 }
               />
               <RFAPositionCard1
                 posHeader={"REQUIREMENTS"}
                 posImage={Laptop}
                 posDescription={
-                  <ul>
-                    <li>
-                      9th grade or above (exceptions may be made, on occasion,
-                      for 8th graders with a volunteering background)
-                    </li>
-                    <li>Consistent access to a computer and stable WiFi</li>
-                    <li>Have skills in one of the following categories:</li>
-                    <ul>
-                      <li>Illustration (Creating Graphics)</li>
-                      <li>Graphic Design (Putting graphics together)</li>
-                    </ul>
-                  </ul>
+                  <Markdown>{pubInternOverviewJSON.requirments}</Markdown>
                 }
               />
               <RFAPositionCard2
                 posHeader={"BENEFICIAL QUALIFICATIONS"}
                 posImage={Presentation}
                 posDescription={
-                  <ul>
-                    <li>Strong communication skills</li>
-                    <li>Good at working with teams</li>
-                    <li>Strong work ethic and responsibility</li>
-                    <li>Well organized</li>
-                  </ul>
+                  <Markdown>{pubInternOverviewJSON.beneficial_qualifications}</Markdown>
                 }
               />
               <RFAPositionCard1
