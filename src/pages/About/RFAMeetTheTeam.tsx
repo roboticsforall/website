8b53c902980b346ec398f28f73d8f183f9f54ea4
Arvind G <60164133+DevSandBoxx@users.noreply.github.com>
2021-06-30 11:32:08 -0500

    fixing bugs

--- src/pages/About/RFAMeetTheTeam.tsx
+++ src/pages/About/RFAMeetTheTeam.tsx
@@ -78,14 +78,14 @@ export const RFAMeetTheTeam: React.FC = () => {
       <br />
         <h1 style={{color: "#fa8d1f", ...header}}>BOARD OF DIRECTORS</h1>
         <Row style = {yellowRow}>
-            {/* <Carousel
+            <Carousel
                 swipeable={false}
                 draggable={false}
                 responsive={responsive}
                 transitionDuration={500}
                 infinite
             >
-                {bmInfoJSON.board_members_list.map((info, i) => (
+                {/* {bmInfoJSON.board_members_list.map((info, i) => (
                     <div key = {i} style = {imgSize} className = "d-flex">
                         <Col md = {"auto"}>
                             <Image style = {imgSize} src = {info.image.replace("/public","")} />
@@ -96,9 +96,9 @@ export const RFAMeetTheTeam: React.FC = () => {
                             <Markdown style = {bodyText}>{info.description}</Markdown>
                         </Col>
                     </div>
-                ))}
+                ))} */}
 
-            </Carousel> */}
+            </Carousel>
         </Row>
         {/* <br/>
         <br/>
