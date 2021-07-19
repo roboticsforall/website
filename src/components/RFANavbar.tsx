1bc5724f6aa2581a5aba7394c915eafc2284b6bd
Arvind G <60164133+DevSandBoxx@users.noreply.github.com>
2021-07-19 11:01:34 -0500

    created nav dropdowns

--- src/components/RFANavbar.tsx
+++ src/components/RFANavbar.tsx
@@ -19,37 +19,138 @@ export const RFANavbar: React.FC = () => {
   const navContainer:React.CSSProperties = {
     display: "flex",
     border: "2px solid black",
-    alignItems: 'center'
   }
-  const navItems:React.CSSProperties = {
-    display: "flex",
-    alignItems: 'center',
-    justifyContent: "space-between",
-    flexGrow: 100,
-    border: "2px solid black",
+  const list:React.CSSProperties = {
     textDecoration: "none",
     listStyleType: "none",
     margin: 0,
-    padding: 0
+    padding: 0,
   }
+  const navItemsList:React.CSSProperties = {
+    display: "flex",
+    alignItems: 'stretch',
+    justifyContent: "space-between",
+    flexGrow: 100,
+  }
+  // const navItem:
 
   return (
-    <nav style = {navContainer}>
+    <nav className = "h-75" style = {navContainer}>
       <header style = {navHeader}>
         <Link to = "/"><img className = "mx-2" src = {TobyHeaderLogo} style = {logoImageSize}/></Link>
         <h1 style = {navHeading}>Robotics For All</h1>
       </header>
-      {/* <div style = {navItemsDiv}> */}
-      <ul className = "ms-2" style = {navItems}>
-        <li>Enroll</li>
-        <li>Volunteer</li>
-        <li>About</li>
-        <li>Contact</li>
-        <li>News</li>
-        <li>Donate</li>
-        <li>Select Language</li>
+      <ul className = "ms-2" style = {{...navItemsList, ...list}}>
+        {[
+          {
+            headerName: "Enroll",
+            to: "/enroll",
+            subPages: [
+              {
+                subPageName: "For Individual Learners",
+                to: "enroll/individlearners",
+              },
+              {
+                subPageName: "For Schools",
+                to: "enroll/schools",
+              },
+              {
+                subPageName: "Register",
+                to: "enroll/register",
+              },
+              {
+                subPageName: "Class FAQs",
+                to: "enroll/classfaq",
+              },
+            ]
+          },
+          {
+            headerName: "Volunteer",
+            to: "/volunteer",
+            subPages: [
+              {
+                subPageName: "Overview",
+                to: "volunteer/overview",
+              },
+              {
+                subPageName: "Teachers Positions",
+                to: "volunteer/teacherpos",
+              },
+              {
+                subPageName: "Publicity Positions",
+                to: "volunteer/publicitypos",
+              },
+              {
+                subPageName: "Volunteer Application",
+                to: "volunteer/volunteerapp",
+              },
+              {
+                subPageName: "Volunteer FAQs",
+                to: "volunteer/volunteerfaq",
+              },
+            ]
+          },
+          {
+            headerName: "Volunteer",
+            to: "/volunteer",
+            subPages: [
+              {
+                subPageName: "Overview",
+                to: "/volunteer/overview",
+              },
+              {
+                subPageName: "Teachers Positions",
+                to: "/volunteer/teacherpos",
+              },
+              {
+                subPageName: "Publicity Positions",
+                to: "/volunteer/publicitypos",
+              },
+              {
+                subPageName: "Volunteer Application",
+                to: "/volunteer/volunteerapp",
+              },
+              {
+                subPageName: "Volunteer FAQs",
+                to: "/volunteer/volunteerfaq",
+              },
+            ]
+          },
+          {
+            headerName: "About",
+            to: "/about",
+            subPages: [
+              {
+                subPageName: "Overview",
+                to: "/about/overview",
+              },
+              {
+                subPageName: "Meet The Team",
+                to: "/about/meettheteam",
+              },
+              {
+                subPageName: "Affiliated Organization",
+                to: "/about/afforgs",
+              },
+              {
+                subPageName: "Newsletter",
+                to: "/about/newsletter",
+              },
+            ]
+          }
+        ].map((navItem, i) => (
+          <React.Fragment>
+            <li className = "nav-item" key = {i}>
+              <h1 className = "h-100 nav-item-header">{navItem.headerName}</h1>
+              <ul className = "dropdown-container" style = {list}>
+                {navItem.subPages.map((subPageInfo, i) => (
+                  <li>{subPageInfo.subPageName}</li>
+                ))}
+              </ul>
+            </li>
+          </React.Fragment>
+        ))}
       </ul>
-      {/* </div> */}
     </nav>
   );
 };
