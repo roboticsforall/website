3b8162ec39fd9791886e5aaf0c6d92d2dad14d03
Michelle Tran <mptran0101@berkeley.edu>
2021-12-19 09:43:17 -0800

    created About Affiliated Orgs Card component and cleaned up AffOrgs styling

--- src/pages/About/AffOrgs.tsx
+++ src/pages/About/AffOrgs.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Header } from "@/components/Header";
-import { Container, Row, Image, Col } from "react-bootstrap";
-import homePageVideo from "@/media/homePageVideo.mp4";
+import { Container } from "react-bootstrap";
 import headerBlobYellow from "@/media/HeaderBlobs/yellow.png";
 import MFALogo from "@/media/AffiliatedOrgs/MFA_Logo.png";
 import CFCLogo from "@/media/AffiliatedOrgs/CFC_Logo.png";
 import TFALogo from "@/media/AffiliatedOrgs/TFA_Logo.png";
 
 import { ColorThemes } from "@/colors";
+
+import { AboutAffOrgsCard } from "@/components/About/AboutAffOrgsCard";
 
 export const AffOrgs: React.FC = () => {
   return (
@@ -64,86 +65,5 @@ export const AffOrgs: React.FC = () => {
       <br />
       <br />
     </div>
-  );
-};
-
-const AboutAffOrgsCard: React.FC<{
-  mainColor: string;
-  affOrgName: string;
-  logo: string;
-  affOrgsDescription: string;
-  website: string;
-  orgAbrv: string;
-  linkColor: string;
-}> = (props) => {
-  const affOrgsCardText = {
-    fontFamily: "BeVietnam-Medium",
-    fontSize: "large",
-  };
-
-  const affOrgsHeader = {
-    fontFamily: "Mazzard-H-Extrabold",
-    color: props.mainColor,
-    fontSize: "3em"
-  };
-
-  return (
-    <Row className="justify-content-center">
-      <Col className="" md={11} lg={11} xl={11}>
-        {window.innerWidth >= 768 && (
-          <Row>
-            <h1 style={affOrgsHeader}>
-              {props.affOrgName}
-            </h1>
-          </Row>
-        )}
-        <Row
-          style={{ backgroundColor: props.mainColor + "80", borderRadius: 15 }}
-        >
-          {window.innerWidth >= 768 ? (
-            <Col sm={3} md={3} lg={3} xl={3} className="p-3 center-align">
-              <Image fluid className="" src={props.logo} />
-            </Col>
-          ) : (
-            <Col>
-              <Row className="p-2">
-                <Col sm = {3} lg = {3} md = {3}  className="center-align">
-                  <Image fluid src={props.logo} />
-                </Col>
-                <Col className="center-align">
-                  <h1 style={affOrgsHeader} className="text-center">
-                    {props.affOrgName}
-                  </h1>
-                </Col>
-              </Row>
-            </Col>
-          )}
-          <Col md={5} lg={5} xl={5} className="p-3 center-align">
-            <video className="w-100 h-100" autoPlay muted loop>
-              <source src={homePageVideo} type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
-          </Col>
-          <Col md={4} lg={4} xl={4} className="p-3">
-            <Container>
-              <Row className="justify-content-center">
-                <p style={affOrgsCardText} className="text-center">
-                  {props.affOrgsDescription}
-                </p>
-                <a
-                  style={{ color: props.linkColor, fontSize: "xx-large" }}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                  className="text-center hyperlink aff-org-card-link"
-                  href={props.website}
-                >
-                  VISIT {props.orgAbrv}
-                </a>
-              </Row>
-            </Container>
-          </Col>
-        </Row>
-      </Col>
-    </Row>
   );
 };
