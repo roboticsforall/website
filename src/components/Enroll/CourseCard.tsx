fa9097c8e9bee526151222468eb81d165d2fcc4e
Arvind G <60164133+DevSandBoxx@users.noreply.github.com>
2022-01-07 21:00:23 -0600

    added modal for the course cards

--- src/components/Enroll/CourseCard.tsx
+++ src/components/Enroll/CourseCard.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Image, Row, Col, Container } from "react-bootstrap";
+import React, { useState } from "react";
+import { Image, Row, Col, Container, Modal, Button, Stack } from "react-bootstrap";
 import Markdown from "markdown-to-jsx";
 import exitIcon from "@/media/Icons/exitIcon.svg";
 import { createUseStyles } from "react-jss";
@@ -15,7 +15,11 @@ interface IProps {
 }
 
 export const CourseCard: React.FC<IProps> = (props: IProps) => {
-  const [dropdownDisplay, setDropdownDisplay] = React.useState("none");
+  const [dropdownDisplay, setDropdownDisplay] = useState("none");
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const text: React.CSSProperties = {
     fontFamily: "Oswald-Medium",
@@ -68,9 +72,7 @@ export const CourseCard: React.FC<IProps> = (props: IProps) => {
         className={classes.cardBackground}
         md={4}
         lg={3}
-        onClick={() => {
-          setDropdownDisplay("flex");
-        }}
+        onClick={handleShow}
       >
         <div className="d-flex justify-content-center">
           <Image fluid src={props.courseImage} />
@@ -79,6 +81,45 @@ export const CourseCard: React.FC<IProps> = (props: IProps) => {
           <h3 style={text}>{props.title}</h3>
         </div>
       </Col>
+
+      <Modal centered fullscreen={"md-down"} size={"lg"} show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            {props.title.toUpperCase()}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="show-grid">
+          <Container>
+            <Row>
+              <Col md={8}>
+                <Markdown>{props.dropdownCourseDescription}</Markdown>
+              </Col>
+              <Col>
+                  <Row className="justify-content-center">
+                    <Image
+                      // fluid
+                      src={props.dropdownCourseImage}
+                    />
+                  </Row>
+                  <Row className="justify-content-center">
+                      <a
+                        style={{ color: props.backgroundColor }}
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        className="hyperlink text-center aff-org-card-link"
+                        href={props.link}
+                      >
+                        Learn More
+                      </a>
+                  </Row>
+              </Col>
+            </Row>
+          </Container>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={handleClose}>Close</Button>
+        </Modal.Footer>
+      </Modal>
 
       <div style={dropdownControl}>
         <div
