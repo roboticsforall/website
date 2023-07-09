fa55eb79c3389df64abc4aedc54bc27e9b3453fb
Arvind Ganeshkumar <arvindram.ganesh@gmail.com>
2023-07-08 21:15:46 -0500

    added redirect to cms:

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import "./custom.scss";
-
 import { App } from "./App";
 import { BrowserRouter, Route } from "react-router-dom";
+import "./index.css";
+import "./custom.scss";
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
