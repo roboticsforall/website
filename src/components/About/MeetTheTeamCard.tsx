50254d9acc3c4bd504733441bf6319bd5370310d
Arvind Ganeshkumar <arvindram.ganesh@gmail.com>
2023-12-22 22:51:48 -0600

    fixed up meet the team page

--- src/components/About/MeetTheTeamCard.tsx
+++ src/components/About/MeetTheTeamCard.tsx
@@ -20,43 +20,29 @@ export const MeetTheTeamCard: React.FC<IProps> = (props: IProps) => {
 
   return (
     <>
-    <div className='holder'>
-      <Col>
-        <Card className="meet-card" onClick={handleShow} 
-              style={{
-                border: 'none', 
-                cursor: 'pointer', 
-                boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 12px',
-                marginBottom: '1rem',
-                padding: '1rem',
-                height: '430px'}}>
-            
-            <Card.Img
-              src={props.info.image + "-/scale_crop/300x350/smart/-/format/webp/-/sharp/10/"}
-              alt={props.info.name}
-            />
-            
-          <Card.Body className="">
-            <Card.Title className="meet-team-card-title" style={{margin: '0'}}>
-              {props.info.name }</Card.Title>
-            <Card.Text className="meet-team-card-text" style={{padding: '5px'}}>
-              {props.info.title_pronouns}
-              </Card.Text>
-          </Card.Body>
-        </Card>
-    </Col>
+      <Card onClick={handleShow} className="h-100 shadow">
+        <Card.Img
+          src={
+            props.info.image +
+            "-/scale_crop/300x350/smart/-/format/webp/-/sharp/10/"
+          }
+          alt={props.info.name}
+        />
 
-     </div>
-    
+        <Card.Body>
+          <Card.Title>{props.info.name}</Card.Title>
+          <Card.Subtitle>{props.info.title_pronouns}</Card.Subtitle>
+        </Card.Body>
+      </Card>
 
-    <Modal 
-      show={show} 
-      onHide={handleClose}
-      centered
-      fullscreen={"md-down"}
-      size={"lg"}
+      <Modal
+        show={show}
+        onHide={handleClose}
+        centered
+        fullscreen={"md-down"}
+        size={"lg"}
       >
-      <Modal.Header closeButton>
+        <Modal.Header closeButton>
           <Modal.Title>{props.info.name}</Modal.Title>
         </Modal.Header>
         <Modal.Body>{props.info.description}</Modal.Body>
--- src/pages/About/MeetTheTeam.tsx
+++ src/pages/About/MeetTheTeam.tsx
@@ -5,96 +5,104 @@ import adInfoJSON from "@/posts/assistant_directors.json";
 //import pcInfoJSON from "@/posts/pub_committee.json";
 
 import { Col } from "react-bootstrap";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
-import { MeetTheTeamCard }  from "@/components/About/MeetTheTeamCard";
+import { MeetTheTeamCard } from "@/components/About/MeetTheTeamCard";
 import { Header } from "@/components/Header";
 
 import { ColorThemes } from "@/colors";
 import headerBlobYellow from "@/media/HeaderBlobs/yellow.png";
 
-
 enum Headers {
-    First = "EXECUTIVE COMMITTEE",
-    Second = "BOARD OF DIRECTORS",
-    Third = "CURRICULUM COMMITTEE",
-    Fourth = "PUBLICITY COMMITTEE",
-    }
+  First = "EXECUTIVE COMMITTEE",
+  Second = "BOARD OF DIRECTORS",
+  Third = "CURRICULUM COMMITTEE",
+  Fourth = "PUBLICITY COMMITTEE",
+}
 
 export const MeetTheTeam: React.FC = () => {
-        
-
-return (
+  return (
     <section>
-        <Header
-            headerTextColor={ColorThemes.mainYellow}
-            image={headerBlobYellow}
-            title={"Get to Know our Team!"}
-            description={"Get to know the faces behind Robotics for All!"} 
-        />
-        <section>
-            <h2 style={{ color: ColorThemes.mainOrange, textAlign: "center", padding: "8px" }}>
-                {Headers.First}
-            </h2>
-            <Container fluid className="container">
-
-                <Row className="d-flex justify-content-center ">
-                    {bmInfoJSON.board_members_list.map((info: any, i: number) => (
-                    <Col xs={10} sm={6} md={4} lg={4} xl={4} key={i}>
-                        <MeetTheTeamCard 
-                        info={info}
-                        backgroundColor={ColorThemes.mainYellow}
-                        color={ColorThemes.darkYellow}
-                    />
-                    </Col>
-                    )
-                )}
-                </Row>
-            </Container>
-        </section>
-
-        <section>
-        <h2 style={{ color: ColorThemes.mainGreen,  textAlign: "center", padding: "8px" }}>
-            {Headers.Second}
+      <Header
+        headerTextColor={ColorThemes.mainYellow}
+        image={headerBlobYellow}
+        title={"Get to Know our Team!"}
+        description={"Get to know the faces behind Robotics for All!"}
+      />
+      <section>
+        <h2
+          style={{
+            color: ColorThemes.mainOrange,
+          }}
+          className="text-center mb-4"
+        >
+          {Headers.First}
         </h2>
-        <Container fluid className="container">
-                <Row className="d-flex justify-content-center">
-                    {adInfoJSON.assistant_directors_list.map((info: any, i: number) => (
-                    <Col xs={12} sm={6} md={6} lg={4} xl={4} key={i}>
-                        <MeetTheTeamCard 
-                        info={info}
-                        backgroundColor={ColorThemes.mainYellow}
-                        color={ColorThemes.darkYellow}
-                    />
-                    </Col>
-                    )
-                )}
-                </Row>
-            </Container>
-        </section>
+        <Container>
+          <Row xs={1} sm={2} md={3} lg={4} xl={4} className="g-4">
+            {bmInfoJSON.board_members_list.map((info: any, i: number) => (
+              <Col key={i}>
+                <MeetTheTeamCard
+                  info={info}
+                  backgroundColor={ColorThemes.mainYellow}
+                  color={ColorThemes.darkYellow}
+                />
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </section>
 
-        <section>
-            <h2 style={{ color: ColorThemes.mainBlue,  textAlign: "center", padding: "8px" }}>
-                {Headers.Third}
-            </h2>
-            <Container fluid className="container">
-                <Row className="d-flex justify-content-center">
-                    {ccInfoJSON.curriculum_committee_list.map((info: any, i: number) => (
-                    <Col xs={12} sm={6} md={6} lg={4} xl={4} key={i} >
-                        <MeetTheTeamCard 
-                        info={info}
-                        backgroundColor={ColorThemes.mainYellow}
-                        color={ColorThemes.darkYellow}
-                    />
-                    </Col>
-                    )
-                )}
-                </Row>
-            </Container>
-        </section>
+      <section>
+        <h2
+          style={{
+            color: ColorThemes.mainGreen,
+          }}
+          className="text-center mb-4"
+        >
+          {Headers.Second}
+        </h2>
+        <Container>
+          <Row xs={1} sm={2} md={3} lg={4} xl={4} className="g-4">
+            {adInfoJSON.assistant_directors_list.map((info: any, i: number) => (
+              <Col key={i}>
+                <MeetTheTeamCard
+                  info={info}
+                  backgroundColor={ColorThemes.mainYellow}
+                  color={ColorThemes.darkYellow}
+                />
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </section>
 
-</section>
-        
-    );
+      <section>
+        <h2
+          style={{
+            color: ColorThemes.mainBlue,
+          }}
+          className="text-center mb-4"
+        >
+          {Headers.Third}
+        </h2>
+        <Container>
+          <Row xs={1} sm={2} md={3} lg={4} xl={4} className="g-4">
+            {ccInfoJSON.curriculum_committee_list.map(
+              (info: any, i: number) => (
+                <Col key={i}>
+                  <MeetTheTeamCard
+                    info={info}
+                    backgroundColor={ColorThemes.mainYellow}
+                    color={ColorThemes.darkYellow}
+                  />
+                </Col>
+              )
+            )}
+          </Row>
+        </Container>
+      </section>
+    </section>
+  );
 };
